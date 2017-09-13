28953befc605dfc55f07e32884a9ceb83783165c
TsiakasK <konstantinos.tsiakas@mavs.uta.edu>
2017-09-13 13:10:25 -0500

    minor

---
+++ PGRL/value_network.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import itertools
+import matplotlib
+import numpy as np
+import sys
+import tensorflow as tf
+import collections
+
+class ValueNetwork():
+    """
+    estimates Q(s,a) 
+    """
+    
+    def __init__(self, learning_rate=0.1, scope="value_estimator"):
+        with tf.variable_scope(scope):
+            self.state = tf.placeholder(tf.int32, [], "state")
+            self.target = tf.placeholder(dtype=tf.float32, name="target")
+
+            # This is just table lookup estimator
+            state_one_hot = tf.one_hot(self.state, int(env.observation_space.n))
+            self.output_layer = tf.contrib.layers.fully_connected(
+                inputs=tf.expand_dims(state_one_hot, 0),
+                num_outputs=1,
+                activation_fn=None,
+                weights_initializer=tf.zeros_initializer)
+
+            self.value_estimate = tf.squeeze(self.output_layer)
+            self.loss = tf.squared_difference(self.value_estimate, self.target)
+
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            self.train_op = self.optimizer.minimize(
+                self.loss, global_step=tf.contrib.framework.get_global_step())        
+    
+    def predict(self, state, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.value_estimate, { self.state: state })
+
+    def update(self, state, target, sess=None):
+        sess = sess or tf.get_default_session()
+        feed_dict = { self.state: state, self.target: target }
+        _, loss = sess.run([self.train_op, self.loss], feed_dict)
+        return loss
