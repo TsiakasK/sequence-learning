33e4103df677523c28c1fb5ec4beb19c5883b076
Konstantinos Tsiakas <konstantinos.tsiakas@mavs.uta.edu>
2017-09-25 14:54:52 -0500

    Update mds.py

--- Data Analysis/mds.py
+++ Data Analysis/mds.py
@@ -15,13 +15,13 @@ import itertools
 seed = np.random.RandomState(seed=3)
 
 
-markers = ['o', 'v', 'h', 'H']
-colors = ['b', 'r','g','c','y','m']
+markers = ['o', 'v', 'h', 'H', 'o', 'v', 'h', 'H', 'h', 'H', 'o', 'v', 'h']
+colors = ['b', 'r','g','c','y','m', 'b', 'r','g','c','y','m','y','m', 'b']
 combs = list(itertools.product(markers, colors))
 
 
 
-dirname = 'data'
+dirname = '../data'
 users = os.listdir(dirname)
 #D = np.zeros((len(pids), 24))
 P = {}
@@ -37,13 +37,14 @@ for user in users:
 		f.close()
 		P = {}
 		scores = []
+		print user + '/' + session
 		for line in lines:
 			a = re.split('\s+', line.strip())
-			level = abs(int(a[-1])) - 1
-			perf = int(a[-2])
-			rf = int(a[-5])
+			level = abs(int(a[3])) - 1
+			perf = int(a[4])
+			rf = int(a[2])
 			key = tuple([level, rf])
-			scores.append(int(a[-1]))
+			scores.append(int(a[3]))
 			if P.has_key(key):
 				P[key].append(perf)	
 			else:
@@ -68,19 +69,23 @@ for user in users:
 		D.append(np.asarray(dd).flatten())
 		
 		um = []
+		print scores
 		for i in [1,2,3,4]: 
 			if scores.count(i) == 0 and scores.count(-1*i) == 0: 
 				um.append(-1.0)
 			else: 
 				um.append(scores.count(i)/float((scores.count(i) + scores.count(-1*i))))
-		print um
+		#print um
 		user_models.append(um)
 
+#print user_models
+"""
 D -= np.asarray(D).mean()
 similarities = euclidean_distances(D)
 mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed,dissimilarity="precomputed", n_jobs=1)
 pos = mds.fit(similarities).embedding_
 plt.subplot(121)
+print pos
 for i, p in enumerate(pos): 
 	plt.plot(p[0], p[1], combs[i][0], markersize=9, color = combs[i][1], label = 'u'+str(i+1))
 plt.title('MDS wrt difficulty and feedback')
@@ -88,12 +93,13 @@ plt.title('MDS wrt difficulty and feedback')
 #plt.show()
 #plt.savefig('performance_RT_mds.jpg', bbox_inches='tight')		
 #plt.close()
-
+"""
+U = user_models
 user_models -= np.asarray(user_models).mean()
 similarities = euclidean_distances(user_models)
 mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed,dissimilarity="precomputed", n_jobs=1)
 pos = mds.fit(similarities).embedding_
-plt.subplot(122)
+#plt.subplot(122)
 for i, p in enumerate(pos): 
 	plt.plot(p[0], p[1], combs[i][0], markersize=9, color = combs[i][1], label = 'u'+str(i+1))
 plt.title('MDS wrt difficulty')
@@ -103,30 +109,16 @@ plt.show()
 plt.close()
 
 
-"""
-		um = []
-		for i in [1.0,2.0,3.0,4.0]: 
-			if scores.count(i) == 0 and scores.count(-1*i) == 0: 
-				um.append(0.0)
-			else: 
-				um.append(scores.count(i)/float((scores.count(i) + scores.count(-1*i))))
-		folders.append(user + '/' + session)
-		user_models.append(um)
-
-
-
-
-
-X = np.asarray(user_models)
-
+X = np.asarray(U)
 
 # CLUSTERING 
+clusters = 3
 
 kmeans = KMeans(n_clusters=clusters, random_state=0).fit(X)
 print kmeans.labels_
 print kmeans.cluster_centers_	
-u.writelines( "%s %s %s\n" % (item1, item2, item3) for item1, item2, item3 in zip(folders, user_models, kmeans.labels_) )
-m.writelines( "%s\n" % item for item in kmeans.cluster_centers_ )
+#u.writelines( "%s %s %s\n" % (item1, item2, item3) for item1, item2, item3 in zip(folders, user_models, kmeans.labels_) )
+#m.writelines( "%s\n" % item for item in kmeans.cluster_centers_ )
 
 if clusters == 1: 
 	index = [[0.5, 3, 5.5 , 8], [1, 3.5, 6, 8.5], [1.5, 4, 6.5, 9], [2, 4.5, 7, 9.5]]
@@ -164,11 +156,10 @@ plt.ylabel('P(success|L)')
 plt.title('User Cluster Centroids (K = '+ str(clusters) +')')
 plt.legend()
 plt.tight_layout()
-plt.savefig('k' + str(clusters)+ '/user_clusters.eps')
+plt.savefig('user_clusters.eps')
 plt.close()
 
-with open('k' + str(clusters) + '/user_models.csv', 'w') as f:
-	writer = csv.writer(f,delimiter=',')
-	writer.writerows(user_models)
-"""
+#with open('k' + str(clusters) + '/user_models.csv', 'w') as f:
+#	writer = csv.writer(f,delimiter=',')
+#	writer.writerows(user_models)
 
