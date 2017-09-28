32f7c9622420e829869dd1c6d6e37710dd66a05d
TsiakasK <konstantinos.tsiakas@mavs.uta.edu>
2017-09-28 16:35:17 -0500

    added

--- Data Analysis/statistical_analysis.py
+++ Data Analysis/statistical_analysis.py
@@ -5,47 +5,67 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
 from pandas.tools.plotting import scatter_matrix
-#import statsmodels.api as sm
-#from statsmodels.formula.api import ols
+from scipy.stats import ttest_ind
 import re
-import seaborn as sns
 
-def correlation_matrix(df):
-    from matplotlib import pyplot as plt
-    from matplotlib import cm as cm
-
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-    cmap = cm.get_cmap('jet', 30)
-    cax = ax1.imshow(df.corr(), interpolation="nearest", cmap=cmap)
-    ax1.grid(True)
-    #plt.title('Abalone Feature Correlation')
-    labels=['engagement', 'length', 'current_result']
-    ax1.set_xticklabels(labels,fontsize=6)
-    ax1.set_yticklabels(labels,fontsize=6)
-    # Add colorbar, make sure to specify tick locations to match desired ticklabels
-    fig.colorbar(cax, ticks=[.75,.8,.85,.90,.95,1])
-    plt.show()
-
-names = ['engagement', 'length', 'current_result']
+# preprocessing
+names = ['cluster','engagement', 'length', 'current_result', 'previous_result', 'action']
 f = open('logfiles/state_action_engagement.csv', 'r')
 lines = f.readlines()
 data = pd.read_csv('logfiles/state_action_engagement.csv', delimiter=' ')
 data[['previous_result', 'current_result']] = data[['previous_result', 'current_result']].astype('float64') 
-C1 = data.loc[data['cluster']==1]
-data = data[['engagement', 'length', 'current_result', 'previous_result']]
-print C1
-correlations = data.corr()
-print correlations
 
-# plot correlation matrix
-fig = plt.figure()
-ax = fig.add_subplot(111)
-cax = ax.matshow(correlations)
-fig.colorbar(cax)
-#ticks = numpy.arange(0,9,1)
-#ax.set_xticks(ticks)
-#ax.set_yticks(ticks)
-ax.set_xticklabels(names)
-ax.set_yticklabels(names)
-plt.show()
+# cluster data
+C0 = data.loc[data['cluster']==0][['engagement', 'length', 'current_result', 'previous_result', 'action']]
+C1 = data.loc[data['cluster']==1][['engagement', 'length', 'current_result', 'previous_result', 'action']]
+C2 = data.loc[data['cluster']==2][['engagement', 'length', 'current_result', 'previous_result', 'action']]
+
+# engagement over failure - over all users
+print "t-test for mean engagement over failure and success"
+E0 = data.loc[data['current_result']==-1][['engagement']]
+E1 = data.loc[data['current_result']==1][['engagement']]
+
+print E0.mean(), E0.var()
+print E1.mean(), E1.var()
+print ttest_ind(E0, E1)
+
+# engagement over failure - over clusters
+print "t-test for mean engagement over failure and success over clusters"
+
+print "cluster0"
+C0E0 = C0.loc[data['current_result']==-1][['engagement']]
+C0E1 = C0.loc[data['current_result']==1][['engagement']]
+print C0E0.mean(), C0E0.var()
+print C0E1.mean(), C0E1.var()
+print ttest_ind(C0E0, C0E1)
+
+print "cluster1"
+C1E0 = C1.loc[data['current_result']==-1][['engagement']]
+C1E1 = C1.loc[data['current_result']==1][['engagement']]
+print C1E0.mean(), C1E0.var()
+print C1E1.mean(), C1E1.var()
+print ttest_ind(C1E0, C1E1)
+
+print "cluster2"
+C2E0 = C2.loc[data['current_result']==-1][['engagement']]
+C2E1 = C2.loc[data['current_result']==1][['engagement']]
+print C2E0.mean(), C2E0.var()
+print C2E1.mean(), C2E1.var()
+print ttest_ind(C2E0, C2E1)
+
+print "engagement for cluster0 vs cluster2 over success"
+print C0E1.mean(), C0E1.var()
+print C2E1.mean(), C2E1.var()
+print ttest_ind(C0E1, C2E1)
+
+print "engagement for cluster0 vs cluster2 over failure"
+print C0E0.mean(), C0E0.var()
+print C2E0.mean(), C2E0.var()
+print ttest_ind(C0E0, C2E0)
+
+## cluster 0,1,2 on levels 1,2,3,4 over win/loss
+
+
+
+
+
