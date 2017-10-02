e2ce891007ac2f5ac9aea6c4515803a32235c152
Maher Abujelala <4708277+abujelala@users.noreply.github.com>
2017-10-02 04:58:17 -0500

    Add files via upload

--- Data Analysis/Performance Model/prepare_performance_model.py
+++ Data Analysis/Performance Model/prepare_performance_model.py
@@ -37,17 +37,107 @@ def actual_label(xFeatures, all_features, all_labels):
         return -1.0
     else:
         return all_labels.loc[data_row.index.values[0]].values[0]
+"""
+def test_NN():
+    #The code used to find the best NN
+    min1 = 1
+    min2 = 1
+    for x in range(2,16):
+        for y in range(2, 16):
+            for solv in [  'sgd','lbfgs', 'adam']:
+                # NN Model
+                NN = MLPRegressor(activation='logistic', max_iter=5000, solver=solv, hidden_layer_sizes=(x, y), random_state=42)
+                NN.fit(strat_train_set, train_labels.values.ravel())
 
+                # Training Accuracy
+                NN_prediction = NN.predict(strat_train_set)
+                NN_mse = mean_squared_error(train_labels, NN_prediction)
+                NN_rmse1 = np.sqrt(NN_mse)
+                #print "\t\tTraining Data: NN Root Mean Square Error = {0:0.2f}".format(NN_rmse1)
 
+                # Testing Accuracy
+                NN_prediction = NN.predict(strat_test_set)
+                NN_mse = mean_squared_error(test_labels, NN_prediction)
+                NN_rmse2 = (np.sqrt(NN_mse))
+                #print "\t\tTesting Data: NN Root Mean Square Error = {0:0.2f}".format(NN_rmse2)
 
+                if min1 > NN_rmse1:
+                    min1 = NN_rmse1
+                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(x,y,solv,NN_rmse1,NN_rmse2)
 
+                if min2 > NN_rmse2:
+                    min2 = NN_rmse2
+                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(x,y,solv,NN_rmse1,NN_rmse2)
+
+def test_SVR():
+# SVR
+    min1 = 1
+    min2 = 1
+    for k in ['linear','poly','rbf','sigmoid']:
+        for c in [0.1, 10.0, 100.0]:
+            for e in [0.001,0.01, 0.1, 0.3, 0.7]:
+                svr = svm.SVR(kernel=k, epsilon=e, tol=1e-3, C=c)
+                svr.fit(strat_train_set, train_labels.values.ravel())
+
+                # Training Accuracy
+                svr_prediction = svr.predict(strat_train_set)
+                svr_mse = mean_squared_error(train_labels, svr_prediction)
+                svr_rmse1 = np.sqrt(svr_mse)
+                #print "\t\tTraining Data: SVR Root Mean Square Error = {0:0.2f}".format(svr_rmse1)
+
+                # Testing Accuracy
+                svr_prediction = svr.predict(strat_test_set)
+                svr_mse = mean_squared_error(test_labels, svr_prediction)
+                svr_rmse2 = np.sqrt(svr_mse)
+                #print "\t\tTesting Data: SVR Root Mean Square Error = {0:0.2f}".format(svr_rmse2)
+
+                if min1 > svr_rmse1:
+                    min1 = svr_rmse1
+                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(c,e,k, svr_rmse1, svr_rmse2)
+
+                if min2 > svr_rmse2:
+                    min2 = svr_rmse2
+                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(c,e,k, svr_rmse1, svr_rmse2)
+
+def test_TR():
+    min1 = 1
+    min2 = 1
+
+    for ss in [2,3,4,5,6,7]:
+        for sl in [ 1, 2,3,4,5,6,7]:
+            for wfl in [0.0, 0.1, 0.2, 0.3, 0.4]:
+                # Decision Tree Regressor
+                tree_reg = DecisionTreeRegressor(random_state=42, min_samples_split = ss, min_samples_leaf=sl, min_weight_fraction_leaf=wfl)
+                tree_reg.fit(strat_train_set, train_labels.values.ravel())
+
+                # Training Accuracy
+                TR_prediction = tree_reg.predict(strat_train_set)
+                TR_mse = mean_squared_error(train_labels, TR_prediction)
+                TR_rmse1 = np.sqrt(TR_mse)
+                #print "\t\tTraining Data: TR Root Mean Square Error = {0:0.2f}".format(TR_rmse1)
+
+                # Testing Accuracy
+                TR_prediction = tree_reg.predict(strat_test_set)
+                TR_mse = mean_squared_error(test_labels, TR_prediction)
+                TR_rmse2 = np.sqrt(TR_mse)
+                #print "\t\tTesting Data: TR Root Mean Square Error = {0:0.2f}".format(TR_rmse2)
+
+                if min1 >= TR_rmse1:
+                    min1 = TR_rmse1
+                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(ss,sl,wfl, TR_rmse1, TR_rmse2)
+
+                if min2 >= TR_rmse2:
+                    min2 = TR_rmse2
+                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(ss,sl,wfl, TR_rmse1, TR_rmse2)
+
+"""
 dirname = '../../sequence-learning-master/Data Analysis/'
 users = os.listdir(dirname)
 data_files_per_cluster = {}
 performance_models_per_cluster = []
 
 # Read the clusters
-clusters_file = dirname + 'clusters'
+clusters_file = dirname + 'datasets/user_clusters.csv'
 c_file = open(clusters_file, 'r')
 lines = c_file.readlines()
 c_file.close()
@@ -107,7 +197,7 @@ print "Successfully saved Performance Models for {} Clusters!".format(len(perfor
 
 """ Regression Model using NN """
 
-"""
+
 rfr_rmse2 = []
 lsvr_rmse2 = []
 nusvr_rmse2 = []
@@ -115,7 +205,7 @@ svr_rmse2 = []
 TR_rmse2 = []
 LR_rmse2 = []
 NN_rmse2 = []
-"""
+
 
 cluster_no = -1
 for cluster in  performance_models_per_cluster:
@@ -137,6 +227,10 @@ for cluster in  performance_models_per_cluster:
 
         strat_test_set = norm_features.loc[test_index]
         test_labels = labels.loc[test_index]
+    #test_NN()
+    #test_SVR()
+    #test_TR()
+    #continue
 
     """
     # Random Forest
@@ -208,7 +302,7 @@ for cluster in  performance_models_per_cluster:
     print "\t\tTesting Data: SVR Root Mean Square Error = {0:0.2f}".format(svr_rmse2[-1])
 
     # Decision Tree Regressor
-    tree_reg = DecisionTreeRegressor(random_state=42, min_samples_leaf=6)
+    tree_reg = DecisionTreeRegressor(random_state=42, min_samples_split = 2, min_samples_leaf=1, min_weight_fraction_leaf=0.2)
     tree_reg.fit(strat_train_set, train_labels.values.ravel())
 
     # Training Accuracy
@@ -240,7 +334,7 @@ for cluster in  performance_models_per_cluster:
     print "\t\tTesting Data: LR Root Mean Square Error = {0:0.2f}".format(LR_rmse2[-1])
 
     # NN Model
-    NN = MLPRegressor( activation='logistic', max_iter=5000, solver='lbfgs', hidden_layer_sizes=(6, 10), random_state=42)
+    NN = MLPRegressor( activation='logistic', max_iter=5000, solver='lbfgs', hidden_layer_sizes=(3, 6), random_state=42)
     NN.fit(strat_train_set,train_labels.values.ravel())
 
 
@@ -256,37 +350,37 @@ for cluster in  performance_models_per_cluster:
     NN_rmse2.append(np.sqrt(NN_mse))
     print "\t\tTesting Data: NN Root Mean Square Error = {0:0.2f}".format(NN_rmse2[-1])
 
-    testing = [sum(rfr_rmse2), sum(lsvr_rmse2), sum(nusvr_rmse2), sum(svr_rmse2), sum(TR_rmse2), sum(LR_rmse2), sum(NN_rmse2)]
-    print [i[0] for i in sorted(enumerate(testing), key=lambda x: x[1])]
-    print ['{0:0.2f}'.format(i[1]) for i in sorted(enumerate(testing), key=lambda x: x[1])]
-
+    #testing = [sum(rfr_rmse2), sum(lsvr_rmse2), sum(nusvr_rmse2), sum(svr_rmse2), sum(TR_rmse2), sum(LR_rmse2), sum(NN_rmse2)]
+    #print [i[0] for i in sorted(enumerate(testing), key=lambda x: x[1])]
+    #print ['{0:0.2f}'.format(i[1]) for i in sorted(enumerate(testing), key=lambda x: x[1])]
+    #exit()
     """
     ####################################################
     ####################################################
-    #The Best Regression Model is Decision Tree Regressor
+    #The Best Regression Model is SVR
     # Decision Tree Regressor
 
-    tree_reg = DecisionTreeRegressor(random_state=42, min_samples_leaf=6)
-    tree_reg.fit(strat_train_set, train_labels.values.ravel())
+    svr = svm.SVR(kernel='rbf', epsilon=0.01, C=10.0)
+    svr.fit(strat_train_set, train_labels.values.ravel())
 
     # Training Accuracy
-    TR_prediction = tree_reg.predict(strat_train_set)
-    TR_mse_train = mean_squared_error(train_labels, TR_prediction)
-    TR_rmse_train = np.sqrt(TR_mse_train)
-    print "\t\tTraining Data: TR Root Mean Square Error = {0:0.2f}".format(TR_rmse_train)
+    svr_prediction = svr.predict(strat_train_set)
+    svr_mse_train = mean_squared_error(train_labels, svr_prediction)
+    svr_rmse_train = np.sqrt(svr_mse_train)
+    print "\t\tTraining Data: SVR Root Mean Square Error = {0:0.2f}".format(svr_rmse_train)
 
     # Testing Accuracy
-    TR_prediction = tree_reg.predict(strat_test_set)
-    TR_mse_test = mean_squared_error(test_labels, TR_prediction)
-    TR_rmse_test = np.sqrt(TR_mse_test)
-    print "\t\tTesting Data: TR Root Mean Square Error = {0:0.2f}".format(TR_rmse_test)
+    svr_prediction = svr.predict(strat_test_set)
+    svr_mse_test = mean_squared_error(test_labels, svr_prediction)
+    svr_rmse_test = np.sqrt(svr_mse_test)
+    print "\t\tTesting Data: SVR Root Mean Square Error = {0:0.2f}".format(svr_rmse_test)
 
     # Figures - Part 1
     fig1 = plt.figure(cluster_no)
     ax1 = fig1.add_subplot(111)
 
 
-    ax1.plot(TR_prediction, c='b', label='Prediction (RMSE={0:0.2f})'.format(TR_rmse_test))
+    ax1.plot(svr_prediction, c='b', label='Prediction (RMSE={0:0.2f})'.format(svr_rmse_test))
     ax1.plot(test_labels.values.tolist(), c='r', label='Actual')
     ax1.set_xlabel('Simulated Samples')
     ax1.set_ylabel('P(Success | Level, Robot Feedback, Prev. Score)')
@@ -299,11 +393,11 @@ for cluster in  performance_models_per_cluster:
 
 
     #Train on all cluster_x Data and save to file
-    tree_reg = DecisionTreeRegressor(random_state=42, min_samples_leaf=6)
-    tree_reg.fit(norm_features, labels.values.ravel())
+    svr_reg = svm.SVR(kernel='rbf', epsilon=0.01, C=10.0)
+    svr_reg.fit(norm_features, labels.values.ravel())
     # You have to create the folder 'Output Models' manually
-    with open('./Output Models/Cluster {} - Decision Tree Regressor Model'.format(cluster_no),'w') as file:
-        pickle.dump(tree_reg,file)
+    with open('./Output Models/Cluster {} - SVR Model'.format(cluster_no),'w') as file:
+        pickle.dump(svr_reg,file)
 
     print "Successfully saved Performance Model for cluster {}".format(cluster_no)
 
@@ -312,13 +406,13 @@ for cluster in  performance_models_per_cluster:
     data_simulation = pd.DataFrame([[l, rf, ps] for l in [0, 1, 2, 3] for rf in [0, 1, 2] for ps in [1, -1, 2, -2, 3, -3, 4, -4]],columns=['Level', 'Robot_Feedback', 'Prev_Score'])
     norm_simulation = normalize_DataFrame(data_simulation.copy())
 
-    prediction = tree_reg.predict(norm_simulation)
+    prediction = svr_reg.predict(norm_simulation)
 
     #Figures - Part 2
     fig1 = plt.figure(cluster_no+3)
     ax1 = fig1.add_subplot(111)
 
-    ax1.plot(range(0, len(prediction)), prediction, c='b', label= 'Prediction (RMSE={0:0.2f})'.format(TR_rmse_test))
+    ax1.plot(range(0, len(prediction)), prediction, c='b', label= 'Prediction (RMSE={0:0.2f})'.format(svr_rmse_test))
 
     first_scatter = True
     for row in norm_simulation.itertuples():
@@ -349,66 +443,3 @@ for cluster in  performance_models_per_cluster:
 
 print "\nEnd of the Program"
 
-""" NN 
-#The code used to find the best NN 
-min1 = 1
-    min2 = 1
-    for x in range(2,16):
-        for y in range(2, 16):
-            for solv in [  'sgd','lbfgs', 'adam']:
-                # NN Model
-                NN = MLPRegressor(activation='logistic', max_iter=5000, solver=solv, hidden_layer_sizes=(x, y), random_state=42)
-                NN.fit(strat_train_set, train_labels.values.ravel())
-
-                # Training Accuracy
-                NN_prediction = NN.predict(strat_train_set)
-                NN_mse = mean_squared_error(train_labels, NN_prediction)
-                NN_rmse1 = np.sqrt(NN_mse)
-                #print "\t\tTraining Data: NN Root Mean Square Error = {0:0.2f}".format(NN_rmse1)
-
-                # Testing Accuracy
-                NN_prediction = NN.predict(strat_test_set)
-                NN_mse = mean_squared_error(test_labels, NN_prediction)
-                NN_rmse2 = (np.sqrt(NN_mse))
-                #print "\t\tTesting Data: NN Root Mean Square Error = {0:0.2f}".format(NN_rmse2)
-
-                if min1 > NN_rmse1:
-                    min1 = NN_rmse1
-                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(x,y,solv,NN_rmse1,NN_rmse2)
-
-                if min2 > NN_rmse2:
-                    min2 = NN_rmse2
-                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(x,y,solv,NN_rmse1,NN_rmse2)
-"""
-
-""" 
-# SVR
-    min1 = 1
-    min2 = 1
-    for k in ['linear','poly','rbf','sigmoid']:
-        for c in [0.1, 10.0, 100.0]:
-            for e in [0.001,0.01, 0.1, 0.3, 0.7]:
-                svr = svm.SVR(kernel=k, epsilon=e, tol=1e-3, C=c)
-                svr.fit(strat_train_set, train_labels.values.ravel())
-
-                # Training Accuracy
-                svr_prediction = svr.predict(strat_train_set)
-                svr_mse = mean_squared_error(train_labels, svr_prediction)
-                svr_rmse1 = np.sqrt(svr_mse)
-                #print "\t\tTraining Data: SVR Root Mean Square Error = {0:0.2f}".format(svr_rmse1)
-
-                # Testing Accuracy
-                svr_prediction = svr.predict(strat_test_set)
-                svr_mse = mean_squared_error(test_labels, svr_prediction)
-                svr_rmse2 = np.sqrt(svr_mse)
-                #print "\t\tTesting Data: SVR Root Mean Square Error = {0:0.2f}".format(svr_rmse2)
-
-                if min1 > svr_rmse1:
-                    min1 = svr_rmse1
-                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(c,e,k, svr_rmse1, svr_rmse2)
-
-                if min2 > svr_rmse2:
-                    min2 = svr_rmse2
-                    print '({0} , {1}) - {2} [{3:0.2f} , {4:0.2f}]'.format(c,e,k, svr_rmse1, svr_rmse2)
-
-"""
